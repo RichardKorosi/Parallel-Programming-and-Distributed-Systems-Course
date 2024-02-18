27e3a61aa56d79a2fc4ea4494adfa5480376a5a3
Richard Körösi <korosi.richard@gmail.com>
2024-02-18 12:30:52 +0100

    docs: add docstrings in the existing functions

--- assignment01.py
+++ assignment01.py
@@ -1,13 +1,26 @@
+"""This file is implementation of the first assignment of the PPDS."""
+
+__author__ = "Richard Körösi"
+
 from fei.ppds import Thread, Semaphore, print
 from time import sleep
 
 
 class Shared:
+    """This class represents shared data."""
+
     def __init__(self):
+        """Initialize shared data."""
         self.semaphore = Semaphore(0)
 
 
 def main():
+    """Create two threads and start them.
+
+    More specifically, this example demonstrates the case in which
+    one thread has to wait for another thread to finish its
+    tasks before it can continue with its own tasks.
+    """
     shared = Shared()
     thread1 = Thread(routine_jano, shared, "Jano")
     thread2 = Thread(routine_fero, shared, "Fero")
@@ -16,14 +29,35 @@ def main():
 
 
 def routine_jano(shared, tid):
+    """Execute the routine of thread Jano.
+
+    Keyword arguments:
+    shared -- shared data between threads
+    tid -- thread identifier
+    """
     sleeping(tid)
 
 
 def routine_fero(shared, tid):
+    """Execute the routine of thread Fero.
+
+    Keyword arguments:
+    shared -- shared data between threads
+    tid -- thread identifier
+    """
     sleeping(tid)
 
 
 def sleeping(tid):
+    """Execute sleeping activity of the thread.
+
+    This function prints a message that the thread is sleeping
+    and then waits for a while before printing a message that the thread
+    is awake.
+
+    Keyword arguments:
+    tid -- thread identifier
+    """
     print(f"{tid} is sleeping.")
     sleep(1)
     print(f"{tid} is awake.")
