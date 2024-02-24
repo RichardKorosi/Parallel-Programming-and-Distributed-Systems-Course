fc0592879a92bc4571a55e7e399716be95a073e4
Richard Körösi <korosi.richard@gmail.com>
2024-02-24 18:07:03 +0100

    feat: create barriers for savages and create cook

--- assignment02.py
+++ assignment02.py
@@ -2,12 +2,12 @@
 
 __author__ = "Richard Körösi"
 
-from colorama import Fore
+from colorama import Fore, Back, Style
 from fei.ppds import Thread, Semaphore, print, Mutex
 from time import sleep
 
 savages = 5
-pot_capacity = 7
+pot_capacity = 3
 
 
 class Shared:
@@ -16,7 +16,8 @@ class Shared:
     def __init__(self):
         """Initialize shared data."""
         self.mutex = Mutex()
-        self.goulash_portions = 0
+        self.goulash_portions = pot_capacity
+        self.waiting_savages = 0
         self.full_pot = Semaphore(0)
         self.empty_pot = Semaphore(0)
 
@@ -31,10 +32,48 @@ def main():
         savage_threads.append(Thread(daily_eating, shared, f"Savage({i})"))
     for i in range(savages):
         savage_threads[i].join()
+    cook = Thread(cook_goulash, shared, "Cook")
+    cook.join()
 
 
 def daily_eating(shared, savage_name):
-    print(f"{Fore.RED}{savage_name} is hungry.")
+    # while True:
+    waiting_room_barrier(shared, savage_name)
+    sleep(1)
+    dinner_table_barrier(shared, savage_name)
+    sleep(1)
+
+
+def waiting_room_barrier(shared, savage_name):
+    shared.mutex.lock()
+    shared.waiting_savages += 1
+    print(f"{Fore.LIGHTMAGENTA_EX}{savage_name} is waiting for others. ({shared.waiting_savages}/{savages})")
+    if shared.waiting_savages == savages:
+        shared.waiting_savages = 0
+        shared.waiting_room.signal(savages)
+    shared.mutex.unlock()
+    shared.waiting_room.wait()
+
+
+def dinner_table_barrier(shared, savage_name):
+    shared.mutex.lock()
+    shared.waiting_savages += 1
+    if shared.waiting_savages == savages:
+        shared.waiting_savages = 0
+        shared.dinner_table.signal(savages)
+    shared.mutex.unlock()
+    shared.dinner_table.wait()
+    print(f"{Fore.YELLOW}{savage_name}: We are all here, let's eat!")
+
+
+def cook_goulash(shared, cook_name):
+    shared.empty_pot.wait()
+    while shared.goulash_portions < pot_capacity:
+        shared.goulash_portions += 1
+        print(f"{Fore.LIGHTBLUE_EX}{cook_name} is cooking.({shared.goulash_portions}/{pot_capacity}).")
+    print(f"{Fore.LIGHTBLUE_EX}{cook_name} has cooked full pot of goulash.")
+    shared.full_pot.signal()
+    shared.empty_pot.wait()
 
 
 if __name__ == '__main__':
