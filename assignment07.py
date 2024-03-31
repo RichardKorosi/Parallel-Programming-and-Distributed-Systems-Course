c58e560070af57923ccb610786e5ddfaf4b5fc13
Richard Körösi <korosi.richard@gmail.com>
2024-03-31 14:19:45 +0200

    feat: add parallel computation using collective communication

--- assignment07.py
+++ assignment07.py
@@ -71,12 +71,45 @@ def p2p_version(nra, nca, ncb):
     print(f"{rank}: Done.")
 
 
+def collective_version(nra, nca, ncb):
+    print(f"{rank}: Starting parallel matrix multiplication example...")
+    print(f"{rank}: Using matrix sizes A[{nra}][{nca}], B[{nca}][{ncb}], C[{nra}][{ncb}]")
+
+    A = None
+    B = None
+    rows = nra // nproc
+    if rank == MASTER:
+        print(f"{rank}: Initializing matrices A and B.")
+        A = np.array([i+j for j in range(nra) for i in range(nca)]).reshape(nproc, nra // nproc, nca)
+        B = np.array([i*j for j in range(nca) for i in range(ncb)]).reshape(nca, ncb)
+
+    A_loc = comm.scatter(A, root = MASTER)
+    B = comm.bcast(B, root = MASTER)
+
+    # Perform sequential matrix multiplication
+    C_loc = np.zeros((rows, ncb), dtype = int)
+    print(f"{rank}: Performing matrix multiplication...")
+    for i in range(rows):
+        for j in range(ncb):
+            for k in range(nca):
+                C_loc[i][j] += A_loc[i][k] * B[k][j]
+
+    # Combine results into matrix C
+    C = comm.gather(C_loc, root = MASTER)
+    if rank == MASTER:
+        C = np.array([ss for s in C for ss in s])
+        print(f"{rank}: Here is the result matrix:")
+        print(C)
+
+    print(f"{rank}: Done.")
+
 def main():
     nra = 32
     nca = 15
     ncb = 7
 
     p2p_version(nra, nca, ncb)
+    collective_version(nra, nca, ncb)
 
 
 
