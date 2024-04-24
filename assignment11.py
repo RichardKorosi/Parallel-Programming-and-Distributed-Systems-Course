b14e05e203ec538b1fa0452960da4874ca196844
Richard Körösi <korosi.richard@gmail.com>
2024-04-24 21:59:08 +0200

    feat: add second dummy task for testing algorithm

--- assignment11.py
+++ assignment11.py
@@ -21,32 +21,47 @@ class Scheduler:
         self.tasks.append(it)
 
     def start(self):
-        data_to_task = ("Hello, Python!", "Hello World!")
+        data_to_task = "Hello, Python?"
         while self.tasks:
             task = self.tasks.pop(0)
             try:
                 task.send(data_to_task)
             except StopIteration:
                 pass
-
 @consumer
 def task1(substring: str):
-    print('Please talk to me!')
     try:
         while True:
             text = yield
             if substring in text:
-                print(f'Oh no: I found a {substring} again!')
+                print(f'T1: I found a {substring}!')
             else:
-                print(f'NICE: I did not find a {substring}!')
+                print(f'T1: I did not find a {substring}!')
     except GeneratorExit:
-        print('Ok, ok: I am quitting.')
+        print('TASK1: GG')
+
+@consumer
+def task2():
+    try:
+        while True:
+            text = yield
+            if "!" in text:
+                print(f'T2: This is order text!')
+            elif "?" in text:
+                print(f'T2: This is question text!')
+            elif "." in text:
+                print(f'T2: This is statement text!')
+            else:
+                print(f'T2: This is unknown text!')
+    except GeneratorExit:
+        print('TASK2: GG')
 
 
 
 def main():
     scheduler = Scheduler()
     scheduler._add_job(task1('Python'))
+    scheduler._add_job(task2())
     scheduler.start()
 
 if __name__ == "__main__":
