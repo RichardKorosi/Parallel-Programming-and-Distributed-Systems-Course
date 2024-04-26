93775a64a3951127a46ba0e356ba4e9f5f134cc8
Richard Körösi <korosi.richard@gmail.com>
2024-04-26 13:49:13 +0200

    feat: add representable jobs and colorful prints

--- assignment11.py
+++ assignment11.py
@@ -1,6 +1,8 @@
 from typing import Callable
 import random
 import string
+from time import sleep
+from colorama import Fore, Style
 
 def consumer(func: Callable) -> Callable:
     def wrapper(*args, **kw):
@@ -16,29 +18,46 @@ def consumer(func: Callable) -> Callable:
 
 class Scheduler:
     def __init__(self):
-        self.jobs = [find_a("a"), get_type()]
+        self.jobs = []
         print('Scheduler is ready!')
 
-    def _add_job(self, it):
-        it.send(''.join(random.choices(string.ascii_lowercase + string.digits, k=10)) + random.choice(['!', '?', '.', ' ']))
+    def add_job(self, it):
+       self.jobs.append(it)
 
     def start(self):
-        try:
-            while True:
+        while True:
+            sleep(0.2)
+            try:
+                data = ''.join(random.choices(string.ascii_lowercase + string.digits, k=10)) + random.choice(['!', '?', '.'])
                 for job in self.jobs:
-                    self._add_job(job)
+                    current_job = job
+                    job.send(data)
                 print('---')
-        except StopIteration:
-            print('Scheduler is done!')
+            except StopIteration:
+                self.jobs.remove(current_job)
+                print(f'\n{Fore.RED}Coprogram {current_job.__name__} has finished!\n')
+            if not self.jobs:
+                break
 
 @consumer
-def find_a(substring: str):
+def two_strings_fight():
     while True:
-        text = yield
-        if substring in text:
-            print(f'T1: I found a {substring}!', text)
+        text1 = yield
+        sum1 = sum(ord(c) for c in text1)
+        print(f'{Fore.LIGHTCYAN_EX}T1: First contestant is: {text1}')
+
+        text2 = yield
+        sum2 = sum(ord(c) for c in text2)
+        print(f'{Fore.LIGHTCYAN_EX}T1: Second contestant is: {text2}')
+
+        if sum1 > sum2:
+            print(f'{Fore.LIGHTCYAN_EX}T1: Contestant one won! {text1} [{sum1}] vs {text2} [{sum2}]')
+        elif sum1 == sum2:
+            print(f'{Fore.LIGHTCYAN_EX}T1: It is a draw! {text1} [{sum1}] vs {text2} [{sum2}]')
         else:
-            print(f'T1: I did not find a {substring}!', text)
+            print(f'{Fore.LIGHTCYAN_EX}T1: Contestant two won! {text1} [{sum1}] vs {text2} [{sum2}]')
+            break
+        
 
 @consumer
 def get_type():
@@ -46,18 +65,35 @@ def get_type():
     while x < 10:
         text = yield
         if "!" in text:
-            print(f'T2: This is order text! {text} ({x+1}/10)')
+            print(f'{Fore.LIGHTGREEN_EX}T2: This is an order! {text} ({x+1}/10)')
         elif "?" in text:
-            print(f'T2: This is question text! {text} ({x+1}/10)')
+            print(f'{Fore.LIGHTGREEN_EX}T2: This is a question! {text} ({x+1}/10)')
         elif "." in text:
-            print(f'T2: This is statement text! {text} ({x+1}/10)')
-        else:
-            print(f'T2: This is unknown text! {text} ({x+1}/10)')
+            print(f'{Fore.LIGHTGREEN_EX}T2: This is a statement! {text} ({x+1}/10)')
         x += 1
+
+@consumer
+def digits_vs_chars():
+    while True:
+        text = yield
+        digits = sum(c.isdigit() for c in text)
+        chars = sum(c.isalpha() for c in text)
+        if digits > chars:
+            print(f'{Fore.LIGHTMAGENTA_EX}T3: Digits won! {text}')
+            break
+        else:
+            print(f'{Fore.LIGHTMAGENTA_EX}T3: Characters won! {text}')
+
 
 
 def main():
     scheduler = Scheduler()
+    job1 = two_strings_fight()
+    job2 = get_type()
+    job3 = digits_vs_chars()
+    scheduler.add_job(job1)
+    scheduler.add_job(job2)
+    scheduler.add_job(job3)
     scheduler.start()
 
 if __name__ == "__main__":
