636881cd4a4f5ae17c347743dd098321e1dcbc45
Richard Körösi <korosi.richard@gmail.com>
2024-05-03 17:35:18 +0200

    feat: convert strings to lists for CUDA

--- sz.py
+++ sz.py
@@ -1,6 +1,7 @@
 import time
 import math
 import warnings
+import string
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -19,7 +20,7 @@ warnings.filterwarnings("ignore", category=NumbaPerformanceWarning)
 
 
 @cuda.jit
-def cuda_kernel(dp_cuda, anti_diagonal, elements_for_thread, i):
+def cuda_kernel(dp_cuda, anti_diagonal, elements_for_thread):
     pos = cuda.grid(1)
     length = len(anti_diagonal)
     start = pos * elements_for_thread
@@ -41,21 +42,27 @@ def cuda_lcs(s1, s2):
     x_string = s1 if len(s1) >= len(s2) else s2
     y_string = s2 if len(s1) >= len(s2) else s1
 
+    x_string = list(x_string.encode('utf-8'))
+    y_string = list(y_string.encode('utf-8'))
+
     dp = np.zeros((len(x_string) + 1, len(y_string) + 1), dtype=np.int32)
     dp_cuda = cuda.to_device(dp)
     no_anti_diagonal = len(x_string) + len(y_string) - 1
     blocks_per_grid = 1
-    elements_for_thread = 2
 
     for i in range(1, no_anti_diagonal + 1):
         x = min(i, len(x_string))
         y = i - x + 1
         anti_diagonal = get_anti_diagonal(x, y, dp)
+        elements_for_thread = min(2, len(anti_diagonal))
         threads_per_block = math.ceil(len(anti_diagonal) / elements_for_thread)
         cuda_kernel[blocks_per_grid, threads_per_block](dp_cuda, anti_diagonal,
-                                                        elements_for_thread, i)
+                                                        elements_for_thread)
 
     dp_result = dp_cuda.copy_to_host()
+
+    print(x_string, bytes(x_string).decode('utf-8'))
+    print(y_string, bytes(y_string).decode('utf-8'))
 
     print(dp_result)
 
