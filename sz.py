28653ba51cfd2b9a0ddbf5b1af30360ad287d706
Richard Körösi <korosi.richard@gmail.com>
2024-05-04 16:18:10 +0200

    feat: add MPI for 3 parallel computations of string combinations

--- sz.py
+++ sz.py
@@ -6,17 +6,16 @@ import string
 import numpy as np
 import matplotlib.pyplot as plt
 
-# from mpi4py import MPI
+from mpi4py import MPI
 from numba import cuda, core
 from numba.core.errors import NumbaPerformanceWarning
 
 warnings.filterwarnings("ignore", category=NumbaPerformanceWarning)
 
-
-# MASTER = 0
-# comm = MPI.COMM_WORLD
-# rank = comm.Get_rank()
-# nproc = comm.Get_size()
+MASTER = 0
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+nproc = comm.Get_size()
 
 
 @cuda.jit
@@ -27,7 +26,6 @@ def cuda_kernel(dp, col_string, row_string, anti_diagonal, elements_for_thread):
     end = min(start + elements_for_thread, length)
 
     for i in range(start, end):
-        # in CUDA row and col are reversed
         row = anti_diagonal[i][0]
         col = anti_diagonal[i][1]
         if col_string[col - 1] == row_string[row - 1] and col_string[col - 1] != 42:
@@ -37,41 +35,37 @@ def cuda_kernel(dp, col_string, row_string, anti_diagonal, elements_for_thread):
 
 
 def main():
-    string1 = "stone"
-    string2 = "longest"
-
     source1 = "**textje********skoro***citatelny******unich"
     source2 = "text*v*tejtoknihe****ma*po***usc*****koniec**robot*rozum"
     source3 = "f*te**xt**sa*je***sko*rio**tu*"
 
-    print(list(source1.encode('utf-8')))
+    list_of_jobs = [(source1, source2), (source1, source3), (source2, source3)]
 
-    source1b = "textjeskorocitatelnyunich"
-    source2b = "textvtejtoknihemapousckoniecrobotrozum"
-    source3b = "ftextsajeskoriotu"
+    if rank == MASTER:
+        final_result = []
 
-    cuda_lcs(string1, string2)
-    print()
-    cuda_lcs(source1b, source2b)
-    print()
-    cuda_lcs(source1b, source3b)
-    print()
-    cuda_lcs(source2b, source3b)
-    print()
-    cuda_lcs(source1, source2)
-    print()
-    cuda_lcs(source1, source3)
-    print()
-    cuda_lcs(source2, source3)
-    print()
+    for i in range(3):
+        if rank == i:
+            result = cuda_lcs(list_of_jobs[i][0], list_of_jobs[i][1])
+
+            if rank == MASTER:
+                final_result.append(result)
+                for j in range(1, nproc):
+                    result = comm.recv(source=j)
+                    final_result.append(result)
+            else:
+                comm.send(result, dest=MASTER)
+
+    if rank == MASTER:
+        print("Max Length:", max(final_result, key=lambda x: x[1]))
 
 
 def cuda_lcs(s1, s2):
     col_string = s1 if len(s1) < len(s2) else s2
     row_string = s2 if len(s1) < len(s2) else s1
 
-    print("col_string:", col_string)
-    print("row_string:", row_string)
+    # print("col_string:", col_string)
+    # print("row_string:", row_string)
 
     col_string = list(col_string.encode('utf-8'))
     row_string = list(row_string.encode('utf-8'))
@@ -99,7 +93,8 @@ def cuda_lcs(s1, s2):
     col_string = bytes(col_string).decode('utf-8')
     row_string = bytes(row_string).decode('utf-8')
 
-    get_result(dp_result, col_string, row_string)
+    result = get_result(dp_result, col_string, row_string)
+    return result
 
 
 def get_anti_diagonal(row, col, dp):
@@ -130,7 +125,7 @@ def get_result(dp, row_string, col_string):
                 row -= 1
                 col -= 1
 
-    print(result, len(result))
+    return result, len(result)
 
 
 if __name__ == '__main__':
