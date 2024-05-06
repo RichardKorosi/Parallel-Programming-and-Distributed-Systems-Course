57d40584a755361dd78f4c6c8f48baf8e0ec3e8b
Richard Körösi <korosi.richard@gmail.com>
2024-05-06 14:27:28 +0200

    style: update code to adhere PEP8 standards

--- sz.py
+++ sz.py
@@ -5,7 +5,6 @@ More info about the assignment can be found in the README.md document.
 
 __author__ = "Richard Körösi"
 
-
 import time
 import math
 import warnings
@@ -27,8 +26,9 @@ nproc = comm.Get_size()
 
 
 @cuda.jit
-def cuda_kernel(dp, col_string, row_string, start_col, start_row, elements_for_thread):
-    """ Calculate one anti-diagonal of the DP matrix in LCS problem.
+def cuda_kernel(dp, col_string, row_string, start_col,
+                start_row, elements_for_thread):
+    """Calculate one anti-diagonal of the DP matrix in LCS problem.
 
     More specifically, this function calculates the values of the DP matrix
     for one anti-diagonal. The anti-diagonal is defined by the starting
@@ -48,7 +48,8 @@ def cuda_kernel(dp, col_string, row_string, start_col, start_row, elements_for_t
     for i in range(elements_for_thread):
         if col < 1 or row > len(dp[0]):
             break
-        if col_string[col - 1] == row_string[row - 1] and col_string[col - 1] != 42:
+        if (col_string[col - 1] == row_string[row - 1]
+                and col_string[col - 1] != 42):
             dp[col, row] = dp[col - 1, row - 1] + 1
         else:
             dp[col, row] = max(dp[col - 1, row], dp[col, row - 1])
@@ -57,17 +58,20 @@ def cuda_kernel(dp, col_string, row_string, start_col, start_row, elements_for_t
 
 
 def main():
-    """ Run the main function of the program.
+    """Run the main function of the program.
 
     More specifically, this function runs the main function of the program.
     It runs the parallel and sequence experiments for the Longest Common
     Subsequence problem of three strings. The results are then compared
     and a graph is created.
     """
-
-    source1 = ["**textje********skoro***citatelny******unich" * i for i in [1, 5, 15, 25, 35, 45]]
-    source2 = ["text*v*tejtoknihe****ma*po***usc*****koniec**robot*rozum" * i for i in [1, 5, 15, 25, 35, 45]]
-    source3 = ["f*te**xt**sa*je***sko*rio**tu*" * i for i in [1, 5, 15, 25, 35, 45]]
+    source1 = ["**textje********skoro***citatelny******unich"
+               * i for i in [1, 5, 15, 25, 35, 45]]
+    source2 = [("text*v*tejtoknihe****ma*po***usc*****"
+                "koniec**robot*rozum")
+               * i for i in [1, 5, 15, 25, 35, 45]]
+    source3 = ["f*te**xt**sa*je***sko*rio**tu*"
+               * i for i in [1, 5, 15, 25, 35, 45]]
 
     experiment_parallel = []
     experiment_sequence = []
@@ -92,8 +96,12 @@ def main():
 
         if rank == MASTER:
             avg_time = sum(times[1:]) / (len(times) - 1)
-            dimensions = f"{len(source1[x])}x{len(source2[x])}x{len(source3[x])}"
-            experiment_parallel.append({"dimensions": dimensions, "time": avg_time})
+            dimensions = (f"{len(source1[x])}"
+                          f"x{len(source2[x])}"
+                          f"x{len(source3[x])}")
+            experiment_parallel.append({
+                "dimensions": dimensions,
+                "time": avg_time})
 
         if rank == MASTER:
             times = []
@@ -103,15 +111,20 @@ def main():
                 times.append(time.perf_counter() - time_start)
 
             avg_time = sum(times[1:]) / (len(times) - 1)
-            dimensions = f"{len(source1[x])}x{len(source2[x])}x{len(source3[x])}"
-            experiment_sequence.append({"dimensions": dimensions, "time": avg_time})
+            dimensions = (f"{len(source1[x])}"
+                          f"x{len(source2[x])}x"
+                          f"{len(source3[x])}")
+            experiment_sequence.append({"dimensions": dimensions,
+                                        "time": avg_time})
 
     if rank == MASTER:
-        create_graph(experiment_parallel, experiment_sequence, info_about_threads)
+        create_graph(experiment_parallel,
+                     experiment_sequence,
+                     info_about_threads)
 
 
 def cuda_lcs(s1, s2, info_about_threads):
-    """ Calculate the Longest Common Subsequence of two strings using CUDA.
+    """Calculate the Longest Common Subsequence of two strings using CUDA.
 
     This function calculates the Longest Common Subsequence of two strings
     using the CUDA parallelization. And returns the result.
@@ -137,7 +150,8 @@ def cuda_lcs(s1, s2, info_about_threads):
 
     threads_per_block = 256
     blocks_per_grid = math.floor(cuda_cores / (threads_per_block * 3))
-    elements_for_thread = math.ceil(no_anti_diagonal / (blocks_per_grid * threads_per_block))
+    elements_for_thread = math.ceil(no_anti_diagonal /
+                                    (blocks_per_grid * threads_per_block))
     info_about_threads["threads"] = threads_per_block * blocks_per_grid
     info_about_threads["blocks_per_grid"] = blocks_per_grid
     info_about_threads["threads_per_block"] = threads_per_block
@@ -147,7 +161,8 @@ def cuda_lcs(s1, s2, info_about_threads):
         row = i - col + 1
 
         (cuda_kernel[blocks_per_grid, threads_per_block]
-         (dp_cuda, col_string_cuda, row_string_cuda, col, row, elements_for_thread))
+         (dp_cuda, col_string_cuda, row_string_cuda,
+          col, row, elements_for_thread))
 
     dp_result = dp_cuda.copy_to_host()
     col_string = bytes(col_string).decode('utf-8')
@@ -158,10 +173,11 @@ def cuda_lcs(s1, s2, info_about_threads):
 
 
 def get_result(dp, row_string, col_string):
-    """ Get the result of the LCS problem.
+    """Get the result of the LCS problem.
 
-    More specifically, this function returns the longest common subsequence
-    of two strings. The result is calculated from the DP matrix.
+    More specifically, this function returns the longest
+    common subsequence of two strings.
+    The result is calculated from the DP matrix.
 
     Keyword arguments:
     dp -- the DP matrix
@@ -190,10 +206,11 @@ def get_result(dp, row_string, col_string):
 
 
 def sequence_lcs(s1, s2):
-    """ Calculate the Longest Common Subsequence of two strings.
+    """Calculate the Longest Common Subsequence of two strings.
 
-    This function calculates the Longest Common Subsequence of two strings
-    using the dynamic programming approach. And returns the result.
+    This function calculates the Longest Common Subsequence
+    of two strings using the dynamic programming approach.
+    And returns the result.
 
     Keyword arguments:
     s1 -- the first string
@@ -202,11 +219,13 @@ def sequence_lcs(s1, s2):
     col_string = s1 if len(s1) < len(s2) else s2
     row_string = s2 if len(s1) < len(s2) else s1
 
-    dp = np.zeros((len(col_string) + 1, len(row_string) + 1), dtype=np.int32)
+    dp = np.zeros((len(col_string) + 1,
+                   len(row_string) + 1), dtype=np.int32)
 
     for i in range(1, len(col_string) + 1):
         for j in range(1, len(row_string) + 1):
-            if col_string[i - 1] == row_string[j - 1] and col_string[i - 1] != "*":
+            if (col_string[i - 1] == row_string[j - 1]
+                    and col_string[i - 1] != "*"):
                 dp[i][j] = dp[i - 1][j - 1] + 1
             else:
                 dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
@@ -217,11 +236,11 @@ def sequence_lcs(s1, s2):
 
 
 def sequence_experiment(list_of_jobs):
-    """ Run the sequence experiment.
+    """Run the sequence experiment.
 
-    More specifically, this function runs the sequence experiment for the
-    Longest Common Subsequence problem of three strings. It runs 3 times
-    and returns the results.
+    More specifically, this function runs the sequence experiment
+    for the Longest Common Subsequence problem of three strings.
+    It runs 3 times and returns the results.
 
     Keyword arguments:
     list_of_jobs -- the list of jobs to run the experiment
@@ -233,25 +252,29 @@ def sequence_experiment(list_of_jobs):
 
 
 def parallel_experiment(list_of_jobs, info_about_threads):
-    """ Run the parallel experiment.
+    """Run the parallel experiment.
 
     More specifically, this function runs the parallel experiment for the
-    Longest Common Subsequence problem of three strings. It runs 3 times
-    and returns the results. The difference between this function and the
+    Longest Common Subsequence problem of three strings.
+    It runs 3 times and returns the results.
+    The difference between this function and the
     sequence_experiment function is that this function uses the CUDA
-    and MPI parallelization. 3 processors are used to run the experiment and run the
+    and MPI parallelization.
+    3 processors are used to run the experiment and run the
     3 jobs in parallel.
 
     Keyword arguments:
     list_of_jobs -- the list of jobs to run the experiment
-    info_about_threads -- the dictionary to store information about threads
+    info_about_threads -- the dictionary to store
+                                        information about threads
     """
     if rank == MASTER:
         final_result = []
 
     for i in range(3):
         if rank == i:
-            result = cuda_lcs(list_of_jobs[i][0], list_of_jobs[i][1], info_about_threads)
+            result = cuda_lcs(list_of_jobs[i][0], list_of_jobs[i][1],
+                              info_about_threads)
 
             if rank == MASTER:
                 final_result.append(result)
@@ -265,11 +288,13 @@ def parallel_experiment(list_of_jobs, info_about_threads):
         print("LCS:", min(final_result, key=lambda x: x[1])[1])
 
 
-def create_graph(experiment_parallel, experiment_sequence, info_about_threads):
-    """ Create a graph of the experiment results.
+def create_graph(experiment_parallel, experiment_sequence,
+                 info_about_threads):
+    """Create a graph of the experiment results.
 
-    More specifically, this function creates a graph of the experiment results.
-    The graph shows the comparison of the parallel and sequence experiments.
+    More specifically, this function creates a graph of the
+    experiment results. The graph shows the comparison
+    of the parallel and sequence experiments.
 
     Keyword arguments:
     experiment_parallel -- the results of the parallel experiment
@@ -285,8 +310,10 @@ def create_graph(experiment_parallel, experiment_sequence, info_about_threads):
 
     fig, ax = plt.subplots()
 
-    rects1 = ax.bar(x - width / 2, time_para, width, label='Parallel', color="r")
-    rects2 = ax.bar(x + width / 2, time_sequence, width, label='Series', color="g")
+    rects1 = ax.bar(x - width / 2, time_para, width,
+                    label='Parallel', color="r")
+    rects2 = ax.bar(x + width / 2, time_sequence, width,
+                    label='Series', color="g")
 
     ax.set_xlabel("Length of three strings")
     ax.set_ylabel("Time [s]")
@@ -298,9 +325,10 @@ def create_graph(experiment_parallel, experiment_sequence, info_about_threads):
     ax.bar_label(rects2, padding=3)
 
     # Add information about threads
-    threads_info = f"Threads for 1 processor: {info_about_threads['threads']}\n" \
-                   f"Threads per block: {info_about_threads['threads_per_block']}\n" \
-                   f"Blocks per grid: {info_about_threads['blocks_per_grid']}"
+    threads_info = \
+        f"Threads for 1 processor: {info_about_threads['threads']}\n" \
+        f"Threads per block: {info_about_threads['threads_per_block']}\n" \
+        f"Blocks per grid: {info_about_threads['blocks_per_grid']}"
 
     # Create a custom legend entry
     from matplotlib.lines import Line2D
